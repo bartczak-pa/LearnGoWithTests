56f0879bb7b7bb13566a37d7c741cdf2397a11d2
Pawel Bartczak <pawel@bartczak.tech>
2024-07-02 13:22:15 +0100

    Added function responsible for iteration

---
+++ iteration/iteration.go
@@ -0,0 +1,11 @@
+package iteration
+
+// Repeat repeats given character given amount of times
+func Repeat(character string, repeatAmount int) string {
+	var repeated string
+	for i := 0; i < repeatAmount; i++ {
+		repeated += character
+	}
+
+	return repeated
+}
