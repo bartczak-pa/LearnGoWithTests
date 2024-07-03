0781e4fc393714cdc7fb66c2d1b96c66ae9d459d
Pawel Bartczak <pawel@bartczak.tech>
2024-07-03 08:49:56 +0100

    Added code for calculating triangle Area

--- structs_methods_interfaces/structs.go
+++ structs_methods_interfaces/structs.go
@@ -1,13 +1,40 @@
 package structsmethodsinterfaces
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
-	height, width float64
+	Height, Width float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.width + rectangle.height)
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+	return rectangle.Width * rectangle.Height
 }
