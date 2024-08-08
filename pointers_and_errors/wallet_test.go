154de5a91c793bfbe822892c830f6af87210e9fc
Pawel Bartczak <pawel@bartczak.tech>
2024-08-08 13:36:50 +0100

    Completed exercise with pointers and errors

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -1,7 +1,6 @@
 package pointers_and_errors
 
 import (
-	"errors"
 	"testing"
 )
 
@@ -10,15 +9,15 @@ func TestWallet(t *testing.T) {
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
 		err := wallet.Withdraw(Bitcoin(10))
-		if err != nil {
-			return
-		}
+
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -36,17 +35,24 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 	got := wallet.Balance()
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
 
-func assertError(t testing.TB, got, want error) {
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertError(t testing.TB, got error, want error) {
 	t.Helper()
 	if got == nil {
 		t.Fatal("didn't get an error but wanted one")
 	}
 
-	if !errors.Is(got, want) {
-		t.Errorf("got %q, want %q", got, want)
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
 	}
 }
