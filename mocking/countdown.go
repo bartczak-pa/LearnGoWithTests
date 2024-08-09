d3b12717c0b096ff208de9fa8183ab7f5f021412
Pawel Bartczak <pawel@bartczak.tech>
2024-08-09 19:46:27 +0100

    Added subtest verifying order of prints and sleeps

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -20,6 +20,7 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
 
@@ -40,3 +41,19 @@ type DefaultSleeper struct{}
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
